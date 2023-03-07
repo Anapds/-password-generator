fe976c9c64698a5705b5982f7ecadfd5a0b6c143
Ana Paula <aninhaaa.paulinha@hotmail.com>
2023-03-07 18:20:56 -0300

    commit

---
+++ password.py
@@ -0,0 +1,35 @@
+import random
+import string
+
+def password_generator(len_pass = 8):
+    ascii_options = string.ascii_letters
+    number_options = string.digits
+    punt_options = string.punctuation
+    options = ascii_options + number_options + punt_options
+
+    password_user = ""
+
+    for i in range(0, len_pass):
+        digit = random.choice(options)
+        password_user = password_user + digit
+
+    
+    return password_user
+
+
+choice_user = input("how many digits are in the password?")
+
+if choice_user.isdigit():
+    choice_user = int(choice_user)
+
+else:
+    print("Invalid Input!")
+    quit()
+
+response = password_generator(len_pass = choice_user)
+print(f"Generated password: \n {response}")
+
+
+    
+
+
